6e0fe0f3ca8fe39f57b41fbc57875787c01d488a
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-18 20:59:49 -0500

    Create router.go

---
+++ infrastructure/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/m3mo89/2022Q2GO-Bootcamp/interface/controller"
+)
+
+func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/pokemons", func(context echo.Context) error { return c.GetPokemons(context) })
+
+	e.GET("/pokemonById/:id", func(context echo.Context) error { return c.GetPokemonById(context) })
+
+	return e
+}
