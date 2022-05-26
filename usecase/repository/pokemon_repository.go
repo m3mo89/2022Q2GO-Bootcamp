f1c03a423f3e0e97614c4ad1a1cc7bc47ae3fc30
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-25 22:23:40 -0500

    Delete pokemon_repository.go

--- usecase/repository/pokemon_repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/m3mo89/2022Q2GO-Bootcamp/domain/model"
-
-type PokemonRepository interface {
-	FindAll() ([]*model.Pokemon, error)
-	FindById(id int) (*model.Pokemon, error)
-}
