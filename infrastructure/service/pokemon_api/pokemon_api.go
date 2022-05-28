92e156a36bef644bdacd9b2b1ddd24caf4e5af74
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-28 16:23:38 -0500

    Create pokemon_api.go

---
+++ infrastructure/service/pokemon_api/pokemon_api.go
@@ -0,0 +1,35 @@
+package pokemon_api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/m3mo89/2022Q2GO-Bootcamp/domain/model"
+)
+
+const apiUrl = "https://pokeapi.co/api/v2/"
+
+func GetPokemon(id int) (*model.Pokemon, error) {
+
+	var pokemon *model.Pokemon
+	pokemonId := strconv.Itoa(id)
+
+	endpoint := "pokemon/" + pokemonId
+
+	resp, err := http.DefaultClient.Get(apiUrl + endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("unable to reach [%v]: %v", apiUrl, err)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read response body: %v", err)
+	}
+
+	json.Unmarshal(content, &pokemon)
+
+	return pokemon, nil
+}
