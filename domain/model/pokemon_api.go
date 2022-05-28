e8c0bdab6c9714e7f3ea0f0f65c70d8b59eadf1e
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-28 16:48:35 -0500

    Create pokemon_api.go

---
+++ domain/model/pokemon_api.go
@@ -0,0 +1,12 @@
+package model
+
+type PokemonApi struct {
+	Id                     uint64 `json:"id"`
+	Name                   string `json:"name"`
+	Height                 int    `json:"height"`
+	IsDefault              bool   `json:"is_default"`
+	Order                  int    `json:"order"`
+	Weight                 int    `json:"weight"`
+	BaseExperience         int    `json:"base_experience"`
+	LocationAreaEncounters string `json:"location_area_encounters"`
+}
