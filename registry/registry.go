b0a5cef049fb6dcc88210cf8348e1bcd05d1bce9
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-28 23:13:37 -0500

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -4,18 +4,20 @@ import (
 	"github.com/m3mo89/2022Q2GO-Bootcamp/interface/controller"
 
 	"github.com/m3mo89/2022Q2GO-Bootcamp/infrastructure/datastore"
+	"github.com/m3mo89/2022Q2GO-Bootcamp/infrastructure/service/pokemon_service"
 )
 
 type registry struct {
-	db datastore.Database
+	db      datastore.Database
+	service pokemon_service.PokemonService
 }
 
 type Registry interface {
 	NewAppController() controller.AppController
 }
 
-func NewRegistry(db datastore.Database) Registry {
-	return &registry{db}
+func NewRegistry(db datastore.Database, service pokemon_service.PokemonService) Registry {
+	return &registry{db, service}
 }
 
 func (r *registry) NewAppController() controller.AppController {
