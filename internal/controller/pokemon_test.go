e26270902b3bc6173595acd8f0a216c87ad4cda4
Guillermo Tinoco <m3mo89@gmail.com>
2022-06-08 23:16:14 -0500

    Update pokemon_test.go

--- internal/controller/pokemon_test.go
+++ internal/controller/pokemon_test.go
@@ -57,3 +57,55 @@ func TestPokemonController_GetPokemons(t *testing.T) {
 		})
 	}
 }
+
+func TestPokemonController_GetPokemonById(t *testing.T) {
+	t.Helper()
+
+	cases := map[string]struct {
+		arrange func(t *testing.T) (PokemonController, Context)
+		assert  func(t *testing.T, context Context, err error)
+	}{
+		"success": {
+			arrange: func(t *testing.T) (PokemonController, Context) {
+				mockService := customMock.NewPokemonServiceMock()
+				mockService.On("GetById", 7).Return(testdata.Pokemons[0], nil)
+				controller := NewPokemonController(mockService)
+
+				mockContext := customMock.NewContextMock()
+				mockContext.On("Param", "id").Return("7")
+				mockContext.On("JSON", http.StatusOK, testdata.Pokemons[0]).Return(nil)
+				return controller, mockContext
+			},
+			assert: func(t *testing.T, context Context, err error) {
+				assert.Nil(t, err)
+				assert.NotNil(t, context)
+				assert.Equal(t, http.StatusOK, 200)
+			},
+		},
+		"error": {
+			arrange: func(t *testing.T) (PokemonController, Context) {
+				mockService := customMock.NewPokemonServiceMock()
+				mockService.On("GetById", 40).Return(nil, errors.New("fake error"))
+				controller := NewPokemonController(mockService)
+
+				mockContext := customMock.NewContextMock()
+				mockContext.On("Param", "id").Return("40")
+				mockContext.On("JSON", http.StatusOK, nil).Return(nil)
+				return controller, mockContext
+			},
+			assert: func(t *testing.T, context Context, err error) {
+				assert.NotNil(t, err)
+				assert.NotNil(t, context)
+				assert.Equal(t, http.StatusOK, 200)
+			},
+		},
+	}
+
+	for k, tt := range cases {
+		t.Run(k, func(t *testing.T) {
+			controller, context := tt.arrange(t)
+			err := controller.GetPokemonById(context)
+			tt.assert(t, context, err)
+		})
+	}
+}
